e34851a59550b869813253c76df8e8eb8f0402d6
Marcin <marcinkondol@gmail.com>
2019-01-30 18:20:47 +0000

    Remove link data

--- src/index.js
+++ src/index.js
@@ -12,19 +12,6 @@ import './styles/base.scss';
 
 const store = configureStore();
 
-store.dispatch(addLink({adress: 'https://www.facebook.com/', description: 'portal spolecznosciowy ze zjebanymi mordami ktorych nie chce widziec'}));
-
-store.dispatch(addLink({adress: 'https://www.youtube.com/', description: 'filmiki sobie mozna tam poogladac', createdAt: 1000}));
-
-store.dispatch(addLink({adress: 'http://mkportfolio.uk/', description: 'najlepsze portfolio pod sloncem'}));
-
-
-const state = store.getState();
-const visibleLinks = getVisibleLinks(state.links, state.filters);
-console.log(visibleLinks);
-
-//console.log(store.getState());
-
 const jsx = (
     <Provider store={store}>
         <AppRouter />
